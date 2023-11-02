35f0c72664dcfef8811ddc22408e03b2cef07c3d
Your Name <you@example.com>
2023-11-02 12:25:17 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,18 @@ import React, { useEffect, useState, EventTarget } from 'react';
 
 export default function Home() {
 
+	const cliqueur_mode = (event) => {
+		let mode = document.querySelector(".mode.selected")
+		if (event.target.className == "mode")
+		{
+			event.target.className = "mode selected"
+			mode.className = "mode"
+			console.log(mode)
+			if (event.target.children == "Easy ")
+				console.log("easy money !!!!!!!")
+		}
+	};
+
 	const cliqueur = (event) => {
 		event.target.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 	  };
@@ -18,14 +30,21 @@ export default function Home() {
 		}, [])
 	}, [])
 
+	useEffect(() => {
+		let mode = document.querySelectorAll(".mode");
+		mode.forEach((mode) => {
+			mode.addEventListener('click', cliqueur_mode);
+		}, [])
+	}, [])
+
   return (
 	// https://codepen.io/itsmhuang/pen/oxaReK
 <div>
 	<h1>The Great <span id="color-display">RGB</span> Guessing Game</h1>
 	<div id ="stripe">
-		<button id="reset">New Colors</button>
+		<button id="reset">Start</button>
 		<span id="message"></span>
-		<button className="mode">Easy</button>
+		<button className="mode">Easy </button>
 		<button className="mode selected">Hard</button>
 	</div>
 	<div id="container">
