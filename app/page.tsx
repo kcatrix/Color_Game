49ee81892cbceb839375c04234f228607b77405c
Your Name <you@example.com>
2023-11-02 17:09:04 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,82 @@
 'use client';
 
+import { Vollkorn } from 'next/font/google';
 import Image from 'next/image'
 import React, { useEffect, useState, EventTarget } from 'react';
 
 
 export default function Home() {
 
-	const cliqueur_mode = (event) => {
-		let mode = document.querySelector(".mode.selected")
-		if (event.target.className == "mode")
+	let mode_de_jeux = 'no game'
+	let mode_enable = 'Hard'
+	let number_of_victory = 0
+
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * max);
+	  }
+	async function boucle(number_of_victory, square, solution, i) {
+
+		console.log("on tien le bon bout ", number_of_victory)
+		setTimeout(() => {
+			solution.push(getRandomInt(5))
+			square[solution[i]].animate(
+				[
+					{ transform: 'scale(1)' },
+					{ transform: 'scale(1.3)' }
+				],
+				{
+					duration: 1000,
+				}
+			);
+			console.log("Delayed for 1 second.");
+			if (number_of_victory != 0)
+		  		boucle(number_of_victory - 1, square, solution, i + 1)
+		  }, 1000);	
+	}
+
+	const cliqueur_start = (event) => {
+		if (mode_de_jeux == 'in game')
+			return 
+		mode_de_jeux = 'in game'
+		let square = document.querySelectorAll(".square");
+		let solution = []
+		if (mode_enable == 'Hard')
 		{
+			console.log("allo la france")
+			boucle(number_of_victory + 3, square, solution, 0);
+			console.log("je veut attendre ")
+		}	
+	}
+
+	const cliqueur_mode = (event) => {
+		if (event.target.className == "mode" && mode_de_jeux == 'no game')
+		{
+			let mode = document.querySelector(".mode.selected")
 			event.target.className = "mode selected"
 			mode.className = "mode"
-			console.log(mode)
-			if (event.target.children == "Easy ")
-				console.log("easy money !!!!!!!")
+			if (event.target.firstChild.textContent == "Easy ")
+			{
+				let square = document.querySelectorAll(".square");
+				let val = square.length / 2
+				for (let i = square.length - 1; i != val - 1; i--)
+					square[i].style.visibility = 'hidden'
+				mode_enable = 'Easy'
+			}
+			else
+			{
+				let square = document.querySelectorAll(".square");
+				let val = square.length
+				let clone = square[0]
+				for (let i = 3; i != val; i++)
+					square[i].style.visibility = 'visible'
+				mode_enable = 'Hard'
+			}
 		}
 	};
 
 	const cliqueur = (event) => {
-		event.target.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+		if (mode_de_jeux == 'no game')
+			event.target.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 	  };
 
 	useEffect(() => {
@@ -37,12 +94,16 @@ export default function Home() {
 		}, [])
 	}, [])
 
+	useEffect(() => {
+		let start = document.querySelector("#start")
+		start?.addEventListener('click', cliqueur_start);
+	}, [])
   return (
 	// https://codepen.io/itsmhuang/pen/oxaReK
 <div>
 	<h1>The Great <span id="color-display">RGB</span> Guessing Game</h1>
 	<div id ="stripe">
-		<button id="reset">Start</button>
+		<button id="start">Start</button>
 		<span id="message"></span>
 		<button className="mode">Easy </button>
 		<button className="mode selected">Hard</button>
